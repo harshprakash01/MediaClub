16c15ccbccb4b2aa31c0c661be3d21687dc0e70a
Harsh <harsh.@Harshs-MacBook-Air.local>
2024-06-17 05:38:30 +0530

    updated team

--- src/HelperComponents/TeamCard.jsx
+++ src/HelperComponents/TeamCard.jsx
@@ -15,11 +15,13 @@ const TeamCard = ({ imageSrc, altText, name, role,link }) => {
             </a>
             <div className="card-content-wrapper">
                 <div className="card-content">
-                    <h3>{name}</h3>
+                    <h3 style={{fontSize: '1.2rem'}}>{name}</h3>
                     <p>{role}</p>
                     <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                             stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round"
+                                  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"/>
                         </svg>
                     </div>
                 </div>
