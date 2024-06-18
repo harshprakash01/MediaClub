dd7608560ddb96c7a52c0983d9c5d985bd7c61ba
Harsh <harsh.@Harshs-MacBook-Air.local>
2024-06-18 13:32:13 +0530

    removed arrow from team

--- src/HelperComponents/TeamCard.jsx
+++ src/HelperComponents/TeamCard.jsx
@@ -18,11 +18,11 @@ const TeamCard = ({ imageSrc, altText, name, role,link }) => {
                     <h3 style={{fontSize: '1.2rem'}}>{name}</h3>
                     <p>{role}</p>
                     <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                             stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"/>
-                        </svg>
+                        {/*<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"*/}
+                        {/*     stroke="currentColor" className="w-6 h-6">*/}
+                        {/*    <path strokeLinecap="round" strokeLinejoin="round"*/}
+                        {/*          d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"/>*/}
+                        {/*</svg>*/}
                     </div>
                 </div>
             </div>
