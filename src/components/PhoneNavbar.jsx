0c7c5880a93cae1290cf39c8a6cf0bdfa685b868
Harsh <harshprakash06@outlook.com>
2024-11-25 01:11:21 +0530

    lie

--- src/components/PhoneNavbar.jsx
+++ src/components/PhoneNavbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import '../CSS/PhoneNavbar.css';
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 const PhoneNavbar = () => {
     const [isActive, setIsActive] = useState(false);
@@ -9,6 +8,7 @@ const PhoneNavbar = () => {
     const toggleMenu = () => {
         setIsActive(!isActive);
     };
+
     const handleClickScroll = (id) => {
         if (window.location.pathname === '/') {
             toggleMenu();
@@ -16,15 +16,16 @@ const PhoneNavbar = () => {
             if (element) {
                 element.scrollIntoView({ behavior: "smooth" });
             }
-        } else { toggleMenu();
+        } else {
+            toggleMenu();
             const newPath = `/#${id}`; // Construct the new path by appending the id
             window.history.pushState(null, '', newPath);
         }
     };
 
     return (
-        <div className="wrap phone-headder">
-            <a href={"/"}><img src={"/assets/logo.png"} alt="logo" className="phone-nav-logo"/></a>
+        <div className={`wrap phone-headder ${isActive ? 'active' : ''}`}> {/* Add the active class here */}
+            <a href={"/"}><img src={"/assets/logo.png"} alt="logo" className="phone-nav-logo" /></a>
             <div className={`burger ${isActive ? 'active' : ''}`} onClick={toggleMenu}>
                 <div className="strip burger-strip-2">
                     <div></div>
@@ -39,18 +40,15 @@ const PhoneNavbar = () => {
                         <div></div>
                         <div></div>
                     </div>
-                </div >
+                </div>
                 <ul>
                     <li className="text" onClick={() => handleClickScroll("hero-element")}><Link to="/">Home</Link></li>
                     <li className="text" onClick={() => handleClickScroll("about")}><Link>About</Link></li>
-                    {/*<li className="text" onClick={() => handleClickScroll("work")}><Link>Work</Link></li>*/}
                     <li className="text" onClick={() => toggleMenu()}><Link to="/team">Team</Link></li>
                     <li className="text"><Link to="/event">Event</Link></li>
                     <li className="text" onClick={() => handleClickScroll("footer")}><Link to="/">Contact</Link></li>
-
                 </ul>
             </div>
-
         </div>
     );
 };
