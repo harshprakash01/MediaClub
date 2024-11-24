ef7a28aab3b11187119f2ea1c0690049b986f0dc
Harsh <harshprakash06@outlook.com>
2024-11-25 03:16:02 +0530

    li;

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'tailwindcss/tailwind.css';
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
